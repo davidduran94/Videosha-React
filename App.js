3442ef91e11cf595fb4d612ca8455f2467bafece
David <david.duran@kairosds.com>
2018-10-26 13:53:18 -0500

    Soporte a dispositivos con notch

--- App.js
+++ App.js
@@ -9,8 +9,9 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, Image} from 'react-native';
 
-import Home from './src/screens/containers/home'
-
+import Home from './src/screens/containers/home';
+import HeaderX from './src/sections/components/header';
+import SuggestionList from './src/videos/container/suggestion-list';
 
 
 type Props = {};
@@ -18,10 +19,16 @@ export default class App extends Component<Props> {
   render() {
     return (
       <Home>
+        <HeaderX>
+          <Text>
+            .
+          </Text>
+        </HeaderX>
         <Text>Aqui va el header</Text>
         <Text>Aqui va el bisca</Text>
         <Text>Aqui va el categgoris</Text>
         <Text>Aqui va el sugerencias</Text>
+        <SuggestionList></SuggestionList>
       </Home>
     );
   }
---
+++ src/videos/container/suggestion-list.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import {FlatList, Text} from 'react-native';
+
+class SuggestionList extends Component {
+    render(){
+        const list = [
+                { title: 'David', key: '1'},
+                { title: 'DR',key: '2'}
+            ];
+        return (
+            <FlatList
+                data={list}
+                renderItem = { ({item}) => <Text>{item.title}</Text> }
+            />
+        )
+    }
+}
+
+export default SuggestionList;
